64ad00991f3efece748cc496f289d8ddd8ad300e
Elizabeth <elizg77@hotmail.com>
2024-03-11 19:43:27 +0100

    added meta tag and changes in contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,14 +13,15 @@ const Contact = () => {
   })
 
   return (
-    
-   <div className="flex flex-col relative   ">
-    <div className="">
-        <img src="./images/transperant.png" alt="scanngo logo" className='absolute opacity-10 w-full h-[140%] object-fill z-0 pb-20
-        ' />
-        </div>
-        {<div data-aos="zoom-in" className="flex flex-col justify-center md:items-center md:w-full h-[100%]">
-        <p className="text-[#06632e] uppercase font-bold mt-72 md:mt-48 md:pt-32 md:mb-3">
+    <div className="w-full py-16 px-10 mt-32 mx-auto 4xl:pr-[23%] mb-24">
+    <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 ">
+      <img data-aos="flip-left"
+        src="./images/kyl.jpg" 
+        alt="A computer"
+        className="w-[90%] h-[90%]  bg-white relative object-fit rounded-lg sm: mx-auto  "
+      />
+      {<div data-aos="zoom-in" className="flex flex-col justify-center mx-auto md:items-center md:w-full h-[90%]">
+        <p className="text-[#06632e] uppercase font-bold mt-24 md:mt-12 md:pt-12 md:mb-3">
               Kontakta oss
             </p>
             <h1 className="md:text-4xl sm:text-3xl  text-2xl font-bold py-2">
@@ -42,28 +43,29 @@ const Contact = () => {
            
           </p>
           
-            <ReCAPTCHA className="ml-16 mt-8 md:ml-8"
+            <ReCAPTCHA className=" mt-8 md:ml-8"
                   sitekey="6LdH9F4iAAAAAGuqxAVOwTGsRTIa0YDpFJ8f6Ii3"
                   onChange={() => setCaptcha(!captcha)}
                 />
                 {!captcha ? (
                  <a
-                 className="bg-green-700 text-[#FFFFFF] pointer-events-none rounded-md font-medium my-14 mx-auto px-6 py-3  hover:bg-orange-600 mb-36"
+                 className="bg-green-700 text-[#FFFFFF] pointer-events-none rounded-md font-medium my-14 mx-auto px-6 py-3  hover:bg-orange-600 "
                  href="mailto:info@scanngo.se"
                >
                  Skicka email
                </a>
                 ) : (
                   <a
-                  className="bg-green-600 text-[#FFFFFF] rounded-md font-medium my-14 mx-auto px-6 py-3 hover:bg-orange-600 mb-36"
+                  className="bg-green-600 text-[#FFFFFF] rounded-md font-medium my-14 mx-auto px-6 py-3 hover:bg-orange-600 "
                   href="mailto:info@scanngo.se"
                 >
                   Skicka email
                 </a>
                 )}
        </div>}
-       </div>
-     
+    </div>
+  </div>
+         
 
   )
 }
