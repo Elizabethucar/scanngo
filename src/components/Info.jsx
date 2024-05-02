8924a07f5aab30a895b2e94ed90426490f038917
Elizabeth <elizg77@hotmail.com>
2024-05-02 13:44:12 +0200

    removed kockar from text

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
+import React from "react";
 import AOS from "aos";
-import "aos/dist/aos.css"
+import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 const Info = () => {
-  
-  useEffect(()=>{
-    AOS.init({duration:1200})
-  })
+  useEffect(() => {
+    AOS.init({ duration: 1200 });
+  });
 
   return (
-    <div className='bg-slate-200 mb-12 relative'>
-        <div data-aos="zoom-in" className='w-full py-8 text-[#0D124B] px-4'>
-          <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3 relative items-center'>
-          <div className='text-center font-medium mr-4'>
-          <span><img
-            src="images/menu.jpg"
-            className="h-16 pl-2 mt-4 md:h-32 sm:h-16 mx-auto "
-            alt="menu"
-          /></span>
-          <h1 className='font-bold text-green-700'>Brett utbud</h1>
-          <p>Med olika smaker och innehåll: kyckling, kött, pasta & BBQ </p>
+    <div className="bg-slate-200 mb-12 relative">
+      <div data-aos="zoom-in" className="w-full py-8 text-[#0D124B] px-4">
+        <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3 relative items-center">
+          <div className="text-center font-medium mr-4">
+            <span>
+              <img
+                src="images/menu.jpg"
+                className="h-16 pl-2 mt-4 md:h-32 sm:h-16 mx-auto "
+                alt="menu"
+              />
+            </span>
+            <h1 className="font-bold text-green-700">Brett utbud</h1>
+            <p>Med olika smaker och innehåll: kyckling, kött, pasta & BBQ </p>
           </div>
-         {/*  <div className='text-center font-medium mr-4'>
+          {/*  <div className='text-center font-medium mr-4'>
           <span><img
             src="images/gluten.jpg"
             className="h-16 pl-2 mt-1 md:h-32 sm:h-16 mx-auto"
@@ -31,30 +32,35 @@ const Info = () => {
           <h1 className='font-bold text-green-700'>Gluten- & laktosfritt</h1>
           <p>80% glutenfria matlådor 95% laktosfria matlådor</p>
           </div> */}
-          <div className='text-center font-medium mr-4 '>
-          <span><img
-            src="images/prot.jpg"
-            className="h-16 pl-2 mt-0 md:h-32 sm:h-16 mx-auto "
-            alt="protein"
-          /></span>
-          <h1 className='font-bold text-green-700'>Bra näringsvärde</h1>
-          <p>Kalorier och macros (protein, kolhydrater, fett)</p>
+          <div className="text-center font-medium mr-4 ">
+            <span>
+              <img
+                src="images/prot.jpg"
+                className="h-16 pl-2 mt-0 md:h-32 sm:h-16 mx-auto "
+                alt="protein"
+              />
+            </span>
+            <h1 className="font-bold text-green-700">Bra näringsvärde</h1>
+            <p>Kalorier och macros (protein, kolhydrater, fett)</p>
           </div>
-          <div className='text-center font-medium mr-4'>
-          <span><img
-            src="images/god.jpg"
-            className="h-16 pl-2 mt-4 md:h-32 sm:h-16 mx-auto"
-            alt="yummy logo"
-          /></span>
-          <h1 className='font-bold text-green-700'>Vällagad mat</h1>
-          <p>Noga valda råvaror som sammansätts och tillagas av våra kockar</p>
-          </div>
+          <div className="text-center font-medium mr-4">
+            <span>
+              <img
+                src="images/god.jpg"
+                className="h-16 pl-2 mt-4 md:h-32 sm:h-16 mx-auto"
+                alt="yummy logo"
+              />
+            </span>
+            <h1 className="font-bold text-green-700">Vällagad mat</h1>
+            <p>
+              Noga valda råvaror som sammansätts med omtanke, kvalitet och
+              hanverk är viktigt för oss{" "}
+            </p>
           </div>
         </div>
-
       </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Info
+export default Info;
--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,56 +1,121 @@
-import React ,{useState} from 'react'
+import React, { useState } from "react";
 import AOS from "aos";
-import "aos/dist/aos.css"
+import "aos/dist/aos.css";
 import { useEffect } from "react";
 
-
 const Faq = () => {
-  const [showdiv, setShowdiv] = useState(false)
+  const [showdiv, setShowdiv] = useState(false);
   const [showdiv1, setShowdiv1] = useState(false);
   const [showdiv2, setShowdiv2] = useState(false);
-  const [showdiv3, setShowdiv3] = useState(false)
-  const [showdiv4, setShowdiv4] = useState(false)
+  const [showdiv3, setShowdiv3] = useState(false);
+  const [showdiv4, setShowdiv4] = useState(false);
 
-  useEffect(()=>{
-   AOS.init({duration:1200})
- })
+  useEffect(() => {
+    AOS.init({ duration: 1200 });
+  });
 
   return (
-    <div className='w-full bg-white py-16 px-12 mt-12 relative'>
-        <div className='flex flex-col justify-center'>
-          <p className='text-orange-600 uppercase font-bold text-3xl mt-24'>Scan´n Go</p>
-          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2 text-black'>Frågor och svar</h1>
+    <div className="w-full bg-white py-16 px-12 mt-12 relative">
+      <div className="flex flex-col justify-center">
+        <p className="text-orange-600 uppercase font-bold text-3xl mt-24">
+          Scan´n Go
+        </p>
+        <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2 text-black">
+          Frågor och svar
+        </h1>
 
-          <button className="bg-green-700 hover:bg-gradient-to-r from-green-800 via-green-500 to-green-800 shadow-lg text-white  rounded-md font-medium my-6 w-full mx-auto px-6 py-3 z-0" onClick={()=>{setShowdiv(!showdiv)}}>Vart leverar ni?</button>
-       { showdiv &&  <div>
-          <p  data-aos="flip-up" className='text-black py-2'> Vi levererar inom hela Stockholmsområdet och Södertälje.
-          </p>
-          </div>}
-          <button className="bg-green-700 hover:bg-gradient-to-r from-green-800 via-green-500 to-green-800 shadow-lg text-white  rounded-md font-medium my-6 w-full mx-auto px-6 py-3 z-0" onClick={()=>{setShowdiv1(!showdiv1)}}>Hur beställer jag matlådor?</button>
-       { showdiv1 &&  <div>
-          <p data-aos="flip-up" className='text-black py-2'> För närvarande beställer ni genom att skicka mejl till oss med era kontaktuppgifter, hur många matlådor och vilka ni vill beställa. Minimum beställning är 10 lådor. Frakt tillkommer med 59kr inom tullarna och 69kr utanför tullarna. Företag har fri frakt.
-          </p>
-          </div>}
-          <button className="bg-green-700 hover:bg-gradient-to-r from-green-800 via-green-500 to-green-800 shadow-lg text-white   rounded-md font-medium my-6 w-full mx-auto px-6 py-3 z-0" onClick={()=>{setShowdiv2(!showdiv2)}}>Hur fungerar leveransen?</button>
-       { showdiv2 &&  <div>
-          <p data-aos="flip-up" className='text-black py-2'> Till privatpersoner levererar vi varje Måndag eftermiddag och beställningen ska läggas på Fredag innan kl 12. Till företag levererar vi på Tisdagar och beställningen ska läggas på Fredag innan kl 12.
-          </p>
-          </div>}
-          <button className="bg-green-700 hover:bg-gradient-to-r from-green-800 via-green-500 to-green-800 shadow-lg text-white   rounded-md font-medium my-6 w-full mx-auto px-6 py-3 z-0" onClick={()=>{setShowdiv3(!showdiv3)}}>Hur länge håller matlådorna?</button>
-       { showdiv3 &&  <div>
-          <p data-aos="flip-up" className='text-black py-2'> Oöpnnad matlåda håller i 21 dagar i kylen, öppnad håller i 7 dagar i kylen. Man kan givetvis frysa in våra matlådor för längre hållbarhet.
-          </p>
-          </div>}
-          <button className="bg-green-700 hover:bg-gradient-to-r from-green-800 via-green-500 to-green-800 shadow-lg text-white   rounded-md font-medium my-6 w-full mx-auto px-6 py-3 z-0 " onClick={()=>{setShowdiv4(!showdiv4)}}>Hur lång tid ska matlådorna värmas?</button>
-       { showdiv4 &&  <div>
-          <p data-aos="flip-up" className='text-black py-2'> Matlådorna ska värmas i 2 min i microvågsugn på 800W. Vissa av våra matlådor kan ätas kalla (se meny).
-          </p>
-          </div>}
-
-        </div>
+        <button
+          className="bg-green-700 hover:bg-gradient-to-r from-green-800 via-green-500 to-green-800 shadow-lg text-white  rounded-md font-medium my-6 w-full mx-auto px-6 py-3 z-0"
+          onClick={() => {
+            setShowdiv(!showdiv);
+          }}
+        >
+          Vart leverar ni?
+        </button>
+        {showdiv && (
+          <div>
+            <p data-aos="flip-up" className="text-black py-2">
+              {" "}
+              Vi levererar inom hela Stockholmsområdet och Södertälje.
+            </p>
+          </div>
+        )}
+        <button
+          className="bg-green-700 hover:bg-gradient-to-r from-green-800 via-green-500 to-green-800 shadow-lg text-white  rounded-md font-medium my-6 w-full mx-auto px-6 py-3 z-0"
+          onClick={() => {
+            setShowdiv1(!showdiv1);
+          }}
+        >
+          Hur beställer jag matlådor?
+        </button>
+        {showdiv1 && (
+          <div>
+            <p data-aos="flip-up" className="text-black py-2">
+              {" "}
+              För närvarande beställer ni genom att skicka mejl till oss med era
+              kontaktuppgifter, hur många matlådor och vilka ni vill beställa.
+              Minimum beställning är 10 lådor. Frakt tillkommer med 59kr inom
+              tullarna och 69kr utanför tullarna. Företag har fri frakt.
+            </p>
+          </div>
+        )}
+        <button
+          className="bg-green-700 hover:bg-gradient-to-r from-green-800 via-green-500 to-green-800 shadow-lg text-white   rounded-md font-medium my-6 w-full mx-auto px-6 py-3 z-0"
+          onClick={() => {
+            setShowdiv2(!showdiv2);
+          }}
+        >
+          Hur fungerar leveransen?
+        </button>
+        {showdiv2 && (
+          <div>
+            <p data-aos="flip-up" className="text-black py-2">
+              {" "}
+              Till privatpersoner levererar vi varje Måndag eftermiddag och
+              beställningen ska läggas på Fredag innan kl 12. Till företag
+              levererar vi på Tisdagar och beställningen ska läggas på Fredag
+              innan kl 12.
+            </p>
+          </div>
+        )}
+        <button
+          className="bg-green-700 hover:bg-gradient-to-r from-green-800 via-green-500 to-green-800 shadow-lg text-white   rounded-md font-medium my-6 w-full mx-auto px-6 py-3 z-0"
+          onClick={() => {
+            setShowdiv3(!showdiv3);
+          }}
+        >
+          Hur länge håller matlådorna?
+        </button>
+        {showdiv3 && (
+          <div>
+            <p data-aos="flip-up" className="text-black py-2">
+              {" "}
+              Oöpnnad matlåda håller i 21 dagar i kylen, öppnad håller i 7 dagar
+              i kylen. Man kan givetvis frysa in våra matlådor för längre
+              hållbarhet.
+            </p>
+          </div>
+        )}
+        <button
+          className="bg-green-700 hover:bg-gradient-to-r from-green-800 via-green-500 to-green-800 shadow-lg text-white   rounded-md font-medium my-6 w-full mx-auto px-6 py-3 z-0 "
+          onClick={() => {
+            setShowdiv4(!showdiv4);
+          }}
+        >
+          Hur lång tid ska matlådorna värmas?
+        </button>
+        {showdiv4 && (
+          <div>
+            <p data-aos="flip-up" className="text-black py-2">
+              {" "}
+              Matlådorna ska värmas i 3-4 min i microvågsugn på 800W. Vissa av
+              våra matlådor kan ätas kalla (se meny).
+            </p>
+          </div>
+        )}
       </div>
-    
-  )
-}
+    </div>
+  );
+};
 
-export default Faq
+export default Faq;
