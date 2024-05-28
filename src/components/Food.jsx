4af9a62414639162ed2e62a7a9cfbefa6aa86224
Elizabeth <elizg77@hotmail.com>
2024-05-28 17:03:44 +0200

    changes to food data

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,39 +1,45 @@
-import React from 'react'
+import React from "react";
 import AOS from "aos";
-import "aos/dist/aos.css"
+import "aos/dist/aos.css";
 import { useEffect } from "react";
 
-
-const Food= () => {
-  useEffect(()=>{
-  AOS.init({duration:1200})
-  })
+const Food = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1200 });
+  });
 
   return (
-    <div  data-aos="fade-up" className='bg-gradient-to-l from-[#06632e]  to-[#9dc7ae] relative'>
-        <div className='w-full py-16 text-[#0D124B] px-4'>
-          <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3 relative'>
-          <span><img
-            src="images/flankstek.png"
-            className="h-18  p-2 md:pr-6 mt-1 md:h-22 sm:h-22"
-            alt="wallenberg"
-          /></span>
-            <span><img
-            src="images/oxfile.png"
-            className="h-18 p-2 mt-1 md:h-22 sm:h-22"
-            alt="salmon"
-          /></span>
-            <span><img
-            src="images/bristek.png"
-            className="h-18 p-2 md:pl-8 mt-1 md:h-22 sm:h-22"
-            alt="meatballs"
-          /></span>
-          </div>
+    <div
+      data-aos="fade-up"
+      className="bg-gradient-to-l from-[#06632e]  to-[#9dc7ae] relative"
+    >
+      <div className="w-full py-16 text-[#0D124B] px-4">
+        <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3 relative">
+          <span>
+            <img
+              src="images/flankstek.png"
+              className="h-18  p-2 md:pr-6 mt-1 md:h-22 sm:h-22"
+              alt="wallenberg"
+            />
+          </span>
+          <span>
+            <img
+              src="images/oxfile.png"
+              className="h-18 p-2 mt-1 md:h-22 sm:h-22"
+              alt="salmon"
+            />
+          </span>
+          <span>
+            <img
+              src="images/biffstroganoff.png"
+              className="h-18 p-2 md:pl-8 mt-1 md:h-22 sm:h-22"
+              alt="meatballs"
+            />
+          </span>
         </div>
-
       </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Food
+export default Food;
